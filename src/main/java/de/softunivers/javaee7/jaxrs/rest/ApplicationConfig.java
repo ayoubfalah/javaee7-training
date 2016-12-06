16aee5091747478aa624bf8147d22c663a9978ab
Ayoub Falah <ayoub_falah@hotmail.de>
2016-12-06 18:29:59 +0100

    stateless EJBs corresponding to different JPA entities. Each EJB has methods to perform CRUD operations on the JPA entity and convenience query methods. Each EJB is also published as a REST endpoint (@Path). The ApplicationConfig class defines the base path of the REST endpoint. The path for the REST endpoint is the same as the JPA entity class name

---
+++ src/main/java/de/softunivers/javaee7/jaxrs/rest/ApplicationConfig.java
@@ -0,0 +1,36 @@
+
+package de.softunivers.javaee7.jaxrs.rest;
+
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author ayoubfalah
+ */
+@ApplicationPath("webresources") 
+public class ApplicationConfig extends Application 
+{
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(de.softunivers.javaee7.jaxrs.rest.CustomerFacadeREST.class);
+        resources.add(de.softunivers.javaee7.jaxrs.rest.ItemFacadeREST.class);
+        resources.add(de.softunivers.javaee7.jaxrs.rest.PurchaseOrderFacadeREST.class);
+        resources.add(de.softunivers.javaee7.jaxrs.rest.WarehouseFacadeREST.class);
+    }
+
+}
---
+++ src/main/java/de/softunivers/javaee7/jaxrs/rest/WarehouseFacadeREST.java
@@ -0,0 +1,88 @@
+
+package de.softunivers.javaee7.jaxrs.rest;
+
+import de.softunivers.javaee7.entities.Warehouse;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author ayoubfalah
+ */
+@Stateless 
+@Path("warehouse")
+public class WarehouseFacadeREST extends AbstractFacade<Warehouse> 
+{
+
+    @PersistenceContext(unitName = "orderPU")
+    private EntityManager em;
+
+    public WarehouseFacadeREST() {
+        super(Warehouse.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void create(Warehouse entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("id") Integer id, Warehouse entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") Integer id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Warehouse find(@PathParam("id") Integer id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Warehouse> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Warehouse> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+}
